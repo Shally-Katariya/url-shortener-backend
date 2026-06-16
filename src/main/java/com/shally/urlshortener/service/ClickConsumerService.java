07dbb04c6032396bff35d275723250e1fe4ecda7
Shally Katariya <156352038+Shally-Katariya@users.noreply.github.com>
2026-06-16 09:59:29 +0000

    fix: disable Kafka consumer when auto-startup is false

--- src/main/java/com/shally/urlshortener/service/ClickConsumerService.java
+++ src/main/java/com/shally/urlshortener/service/ClickConsumerService.java
@@ -1,46 +1,29 @@
 package com.shally.urlshortener.service;
-
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-
 import com.shally.urlshortener.repository.UrlRepository;
 
 @Service
+@ConditionalOnProperty(name = "spring.kafka.listener.auto-startup", havingValue = "true")
 public class ClickConsumerService {
-
-    // ✅ Thread-safe map
     private final Map<String, Long> clickBuffer = new ConcurrentHashMap<>();
-
     @Autowired
     private UrlRepository urlRepository;
-
     @KafkaListener(topics = "url-events", groupId = "url-shortener-group")
     public void consume(String shortCode) {
-
         clickBuffer.merge(shortCode, 1L, Long::sum);
-
-        System.out.println("Buffered click for: " + shortCode);
     }
-
     @Scheduled(fixedRate = 10000)
     public void flushToDB() {
-
-        // ✅ Avoid unnecessary DB calls
-        if (clickBuffer.isEmpty()) {
-            return;
-        }
-
-        System.out.println("Flushing to DB: " + clickBuffer);
-
+        if (clickBuffer.isEmpty()) return;
         clickBuffer.forEach((shortCode, count) -> {
             urlRepository.incrementClickCount(shortCode, count);
         });
-
         clickBuffer.clear();
     }
 }
